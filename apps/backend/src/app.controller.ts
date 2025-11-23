1edc9db79f53c6e7672b4d2f19faf7e6eb363382
IngsR <ikhwan2ende@gmail.com>
2025-11-23 19:01:41 +0700

    feat:refactor architecture dir to be microservice

---
+++ apps/backend/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller({
+  version: VERSION_NEUTRAL,
+})
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello() {
+    return {
+      message: 'Welcome to CRM API',
+      docs: '/api',
+    };
+  }
+}
